72dac53f6649acfbd05457ee631975eac543661b
clancy <79643779+mxratt@users.noreply.github.com>
2023-03-15 21:36:44 -0400

    morse_encoder fixed
    lol idk how i updated this with the completely wrong script last time but this is the actual morse encoder script

--- morse_encoder.py
+++ morse_encoder.py
@@ -1,159 +1,199 @@
-import time
-import random
 import os
+import time
 
-# set console size
-size = 'mode 180,5'
+size = 'mode 150,5'
 os.system(size)
 
 # default data collection
-output = ""
-cipher = "cipher.log"
-dice = int()
-value = int(1)
-step = 0
-extra = 0
-key = []
-alp = [
-        "A",  # 0
-        "B",  # 1
-        "C",  # 2
-        "D",  # 3
-        "E",  # 4
-        "F",  # 5
-        "G",  # 6
-        "H",  # 7
-        "I",  # 8
-        "J",  # 9
-        "K",  # 10
-        "L",  # 11
-        "M",  # 12
-        "N",  # 13
-        "O",  # 14
-        "P",  # 15
-        "Q",  # 16
-        "R",  # 17
-        "S",  # 18
-        "T",  # 19
-        "U",  # 20
-        "V",  # 21
-        "W",  # 22
-        "X",  # 23
-        "Y",  # 24
-        "Z",  # 25
-        "A",  # 0
-        "B",  # 1
-        "C",  # 2
-        "D",  # 3
-        "E",  # 4
-        "F",  # 5
-        "G",  # 6
-        "H",  # 7
-        "I",  # 8
-        "J",  # 9
-        "K",  # 10
-        "L",  # 11
-        "M",  # 12
-        "N",  # 13
-        "O",  # 14
-        "P",  # 15
-        "Q",  # 16
-        "R",  # 17
-        "S",  # 18
-        "T",  # 19
-        "U",  # 20
-        "V",  # 21
-        "W",  # 22
-        "X",  # 23
-        "Y",  # 24
-        "Z"   # 25
+char = 0
+code = 0
+space = " "
+poof = ""
+file = "morse.log"
+
+# english/digits
+decoded = [
+    "A",   # 0
+    "B",   # 1
+    "C",   # 2
+    "D",   # 3
+    "E",   # 4
+    "F",   # 5
+    "G",   # 6
+    "H",   # 7
+    "I",   # 8
+    "J",   # 9
+    "K",   # 10
+    "L",   # 11
+    "M",   # 12
+    "N",   # 13
+    "O",   # 14
+    "P",   # 15
+    "Q",   # 16
+    "R",   # 17
+    "S",   # 18
+    "T",   # 19
+    "U",   # 20
+    "V",   # 21
+    "W",   # 22
+    "X",   # 23
+    "Y",   # 24
+    "Z",   # 25
+    "0",   # 26
+    "1",   # 27
+    "2",   # 28
+    "3",   # 29
+    "4",   # 30
+    "5",   # 31
+    "6",   # 32
+    "7",   # 33
+    "8",   # 34
+    "9",   # 35
+    ".",   # 36
+    ",",   # 37
+    "?",   # 38
+    "'",   # 39
+    "!",   # 40
+    "/",   # 41
+    "(",   # 42
+    ")",   # 43
+    "&",   # 44
+    ":",   # 45
+    ";",   # 46
+    "=",   # 47
+    "+",   # 48
+    "-",   # 49
+    "_",   # 50
+    "\"",  # 51
+    "$",   # 52
+    "@",   # 53
+    "¿",   # 54
+    "¡",   # 55
 ]
 
-# create or overwrite output.txt
-file = open(cipher, "w")
-file.writelines("")
-file.close()
+# special characters
+special = []
 
-# ask for input
-word = input("input: ").upper()  # convert input to uppercase
-length = len(word)  # get length of input in total character sum
+# full stop, AR prosign meaning "End of Message"
+full_stop = [
+    "|"
+]
 
-# ask for shift
-shift = input("shift: ")
-shift = int(shift)  # converting shift to int
+# characters that will be removed
+trash = [
+    "\\",
+    "<",
+    ">",
+    "%",
+    "^",
+    "*",
+    "~",
+    "`",
+    "{",
+    "}",
+]
 
-while step != length:  # each loop increases step by 1. once step is equal to the length of the input the loop breaks
-    output = word[step]  # splits input into characters, proceeds to the next char each loop
-    dice = random.randint(1, 26)  # roll the dice, gives back a pseudo-random number
+# morse
+encoded = [
+    ".-",       # A
+    "-...",     # B
+    "-.-.",     # C
+    "-..",      # D
+    ".",        # E
+    "..-.",     # F
+    "--.",      # G
+    "....",     # H
+    "..",       # I
+    ".---",     # J
+    "-.-",      # K
+    ".-..",     # L
+    "--",       # M
+    "-.",       # N
+    "---",      # O
+    ".--.",     # P
+    "--.-",     # Q
+    ".-.",      # R
+    "...",      # S
+    "-",        # T
+    "..-",      # U
+    "...-",     # V
+    ".--",      # W
+    "-..-",     # X
+    "-.--",     # Y
+    "--..",     # Z
+    "-----",    # 0
+    ".----",    # 1
+    "..---",    # 2
+    "...--",    # 3
+    "....-",    # 4
+    ".....",    # 5
+    "-....",    # 6
+    "--...",    # 7
+    "---..",    # 8
+    "----.",    # 9
+    ".-.-.-",   # .
+    "--..--",   # ,
+    "..--..",   # ?
+    ".----.",   # '
+    "-.-.--",   # !
+    "-..-.",    # /
+    "-.--.",    # (
+    "-.--.-",   # )
+    ".-...",    # &
+    "---...",   # :
+    "-.-.-.",   # ;
+    "-...-",    # =
+    ".-.-.",    # +
+    "-....-",   # -
+    "..--.-",   # _
+    ".-..-.",   # "
+    "...-..-",  # $
+    ".--.-.",   # @
+    "..-.-",    # ¿
+    "--...-",   # ¡
+]
 
-    # if output isn't in the alp list it'll pass as itself, unencoded
-    if output not in alp and extra != length:
-        output = output
-        extra += 1
-        continue
-    # check which letter = output, then applies the cipher
-    elif output == alp[0]:
-        output = alp[abs(0 + dice)]
-    elif output == alp[1]:
-        output = alp[abs(1 + dice)]
-    elif output == alp[2]:
-        output = alp[abs(2 + dice)]
-    elif output == alp[3]:
-        output = alp[abs(3 + dice)]
-    elif output == alp[4]:
-        output = alp[abs(4 + dice)]
-    elif output == alp[5]:
-        output = alp[abs(5 + dice)]
-    elif output == alp[6]:
-        output = alp[abs(6 + dice)]
-    elif output == alp[7]:
-        output = alp[abs(7 + dice)]
-    elif output == alp[8]:
-        output = alp[abs(8 + dice)]
-    elif output == alp[9]:
-        output = alp[abs(9 + dice)]
-    elif output == alp[10]:
-        output = alp[abs(10 + dice)]
-    elif output == alp[11]:
-        output = alp[abs(11 + dice)]
-    elif output == alp[12]:
-        output = alp[abs(12 + dice)]
-    elif output == alp[13]:
-        output = alp[abs(13 + dice)]
-    elif output == alp[14]:
-        output = alp[abs(14 + dice)]
-    elif output == alp[15]:
-        output = alp[abs(15 + dice)]
-    elif output == alp[16]:
-        output = alp[abs(16 + dice)]
-    elif output == alp[17]:
-        output = alp[abs(17 + dice)]
-    elif output == alp[18]:
-        output = alp[abs(18 + dice)]
-    elif output == alp[19]:
-        output = alp[abs(19 + dice)]
-    elif output == alp[20]:
-        output = alp[abs(20 + dice)]
-    elif output == alp[21]:
-        output = alp[abs(21 + dice)]
-    elif output == alp[22]:
-        output = alp[abs(22 + dice)]
-    elif output == alp[23]:
-        output = alp[abs(23 + dice)]
-    elif output == alp[24]:
-        output = alp[abs(24 + dice)]
-    elif output == alp[25]:
-        output = alp[abs(25 + dice)]
+# create or overwrite morse.txt
+morse = open(file, "w")
+morse.writelines("[ ")
+morse.close()
 
-    # this prevents characters that aren't letters from being keyed
-    if output in alp:
-        dice += shift  # add shift
-        key.append(abs(dice))  # add the newest key list to key list (this gets printed after the loop finishes)
-    print(output, end="")
-    file = open(cipher, "a")
-    file.writelines(output)
-    step += 1
-file.write("\n{}".format(key) + "\n\nsuper secret shift key: " + str(shift))
-file.close()
-print("\n[", *key, "]\nsuper secret shift key:", shift, "\ncheck cipher.log")
+# get input, convert it to uppercase, get length of input and set that as the loop limit
+IN = input("input: ").upper()
+length = len(IN)
+print("[ ", end="")
+# start the loop with step at 0, increasing by 1 until it reaches the length of the input
+while char != length:
+    # set output as input (character by character, moves to next char each loop)
+    OUT = IN[char]
+    # if input is a space, convert it to 3 spaces (value is 2 because each char already comes with a space)
+    if OUT == space:
+        OUT = space * 2
+    # if input character is in the decoded list, it encodes the character
+    elif OUT in decoded:
+        code = decoded.index(OUT)
+        OUT = encoded[code] + space
+    # if input is a special character, it is passed as itself
+    elif OUT in full_stop:
+        OUT = ".-.-. "
+    elif OUT in special:
+        OUT = OUT + space
+    # if input is a character that isn't accepted in morse, it gets removed (or...trashed...lol)
+    elif OUT in trash:
+        OUT = poof
+    # if input char isn't in any valid list, it breaks the loop and throws an error
+    else:
+        print("ERROR!!! INVALID INPUT DETECTED!!!\n\nInvalid Input =", OUT)
+        break
+    # print result character by character each loop until the loop is filled aka char = length
+    print(OUT, end="")
+    # write result to morse.txt, also char by char
+    open(file, "a").writelines(OUT)
+    # move to the next character in the input
+    char += 1
+# when the loop ends, output terminal prints original input, pauses for 15 secs, and then closes
+print("]")
+open(file, "a").writelines("]")
+# close file (good practice <3)
+morse.close()
 time.sleep(300)
